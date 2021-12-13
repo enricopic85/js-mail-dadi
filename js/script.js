bbd78aa648c9121d3f2023d259c7db95c16048e6
enricopic85 <enricodev85@gmail.com>
2021-12-13 15:18:25 +0100

    primo svolgimento

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//1^ esercizio
+
+ const validEmail=["arturo@gino.it","gennarosavastano@gomorra.it","enricopic@live.it"];
+ let emailUser=prompt("inserisci la tua mail");
+ let validUser=false;
+ const nav=document.querySelector("nav");
+ for(let i=0; i < validEmail.length; i++){ 
+         if (emailUser.toLowerCase===validEmail[i].toLowerCase) {
+             validUser=true;
+     } 
+ }
+ if (validUser) {
+     const element=document.createElement("h1");
+     nav.append(element);
+     element.append(emailUser);
+     alert("la tua mail è valida");
+ } else{
+     alert("la tua mail non è valida");
+ }
+
+/* 2^ esercizio Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+// const punteggioUser=Math.floor(Math.random() * 6) + 1;
+// alert(`caro umano, hai fatto ${punteggioUser}!`);
+
+// const punteggioPc=Math.floor(Math.random() * 6) + 1;
+// alert(`cara macchina, hai fatto ${punteggioPc}!`);
+
+// if (punteggioUser > punteggioPc) {
+//     alert("complimenti umano hai sconfitto il computer!")
+// } else if (punteggioUser < punteggioPc) {
+//     alert("cara macchina sei la vincitrice!")
+// } else{
+//     alert("Pareggio!");
+// }
