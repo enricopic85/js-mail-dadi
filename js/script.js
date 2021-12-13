d23ad87e5e61ad8dfda4f1c8352df0f091d91ce7
enricopic85 <enricodev85@gmail.com>
2021-12-13 15:42:09 +0100

    sistemazione esercizi

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@
  const validEmail=["arturo@gino.it","gennarosavastano@gomorra.it","enricopic@live.it"];
  let emailUser=prompt("inserisci la tua mail");
  let validUser=false;
- const nav=document.querySelector("nav");
+ 
  for(let i=0; i < validEmail.length; i++){ 
-         if (emailUser.toLowerCase===validEmail[i].toLowerCase) {
+         if (emailUser.toLowerCase()===validEmail[i].toLowerCase()) {
              validUser=true;
      } 
  }
  if (validUser) {
+     const nav=document.querySelector("nav");
      const element=document.createElement("h1");
      nav.append(element);
      element.append(emailUser);
@@ -20,16 +21,16 @@
 
 /* 2^ esercizio Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
-// const punteggioUser=Math.floor(Math.random() * 6) + 1;
-// alert(`caro umano, hai fatto ${punteggioUser}!`);
+ const punteggioUser=Math.floor(Math.random() * 6) + 1;
+ alert(`caro umano, hai fatto ${punteggioUser}!`);
 
-// const punteggioPc=Math.floor(Math.random() * 6) + 1;
-// alert(`cara macchina, hai fatto ${punteggioPc}!`);
+ const punteggioPc=Math.floor(Math.random() * 6) + 1;
+ alert(`cara macchina, hai fatto ${punteggioPc}!`);
 
-// if (punteggioUser > punteggioPc) {
-//     alert("complimenti umano hai sconfitto il computer!")
-// } else if (punteggioUser < punteggioPc) {
-//     alert("cara macchina sei la vincitrice!")
-// } else{
-//     alert("Pareggio!");
-// }
+ if (punteggioUser > punteggioPc) {
+     alert("complimenti umano hai sconfitto il computer!")
+ } else if (punteggioUser < punteggioPc) {
+     alert("cara macchina sei la vincitrice!")
+ } else{
+     alert("Pareggio!");
+ }
