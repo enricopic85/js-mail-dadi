3bd6a37efe98d521c94618d0e11bcd5ae8fc0e22
enricopic85 <enricodev85@gmail.com>
2021-12-13 21:23:09 +0100

    aggiustamento switch

--- js/script.js
+++ js/script.js
@@ -1,44 +1,45 @@
 //1^ esercizio
 
- const validEmail=["arturo@gino.it","gennarosavastano@gomorra.it","enricopic@live.it"];
- let emailUser=prompt("inserisci la tua mail");
- let validUser=false;
+//  const validEmail=["arturo@gino.it","gennarosavastano@gomorra.it","enricopic@live.it"];
+//  let emailUser=prompt("inserisci la tua mail");
+//  let validUser=false;
  
- for(let i=0; i < validEmail.length; i++){ 
-         if (emailUser.toLowerCase()===validEmail[i].toLowerCase()) {
-             validUser=true;
-     } 
- }
- if (validUser) {
-     const nav=document.querySelector("nav");
-     const element=document.createElement("h1");
-     nav.append(element);
-     element.append(emailUser);
-     alert("la tua mail è valida");
- } else{
-     alert("la tua mail non è valida");
- }
+//  for(let i=0; i < validEmail.length; i++){ 
+//          if (emailUser.toLowerCase()===validEmail[i].toLowerCase()) {
+//              validUser=true;
+//      } 
+//  }
+//  if (validUser) {
+//      const nav=document.querySelector("nav");
+//      const element=document.createElement("h1");
+//      nav.append(element);
+//      element.append(emailUser);
+//      alert("la tua mail è valida");
+//  } else{
+//      alert("la tua mail non è valida");
+//  }
 
 /* 2^ esercizio Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
- const punteggioUser=Math.floor(Math.random() * 6) + 1;
- alert(`caro umano, hai fatto ${punteggioUser}!`);
+//  const punteggioUser=Math.floor(Math.random() * 6) + 1;
+//  alert(`caro umano, hai fatto ${punteggioUser}!`);
 
- const punteggioPc=Math.floor(Math.random() * 6) + 1;
- alert(`cara macchina, hai fatto ${punteggioPc}!`);
+//  const punteggioPc=Math.floor(Math.random() * 6) + 1;
+//  alert(`cara macchina, hai fatto ${punteggioPc}!`);
 
- if (punteggioUser > punteggioPc) {
-     alert("complimenti umano hai sconfitto il computer!")
- } else if (punteggioUser < punteggioPc) {
-     alert("cara macchina sei la vincitrice!")
- } else{
-     alert("Pareggio!");
- }
+//  if (punteggioUser > punteggioPc) {
+//      alert("complimenti umano hai sconfitto il computer!")
+//  } else if (punteggioUser < punteggioPc) {
+//      alert("cara macchina sei la vincitrice!")
+//  } else{
+//      alert("Pareggio!");
+//  }
 
 
  //variante dadi con switch case(da finire)
 const punteggioUser=Math.floor(Math.random() * 6) + 1;
 const punteggioPc=Math.floor(Math.random() * 6) + 1;
+
 switch (punteggioUser) {
     case 1:
         // let punteggio1=document.getElementById("punteggio");
@@ -146,11 +147,12 @@ switch (punteggioPc) {
 
 if (punteggioUser > punteggioPc) {
     let win=document.getElementById("titolo");
-    win.innerHTML("Complimenti umano hai vinto")
+    win.innerHTML="Complimenti umano hai vinto";
 } else if (punteggioUser < punteggioPc) {
    let win=document.getElementById("titolo");
-   win.innerHTML("Complimenti macchina hai vinto")
+   win.innerHTML="Complimenti macchina hai vinto";
 } else{
    let win=document.getElementById("titolo");
-   win.innerHTML("Pareggio!")
+   win.innerHTML="Pareggio!";
 }
+
